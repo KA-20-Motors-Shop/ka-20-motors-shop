2f95c7fcfd824560d4a77155bd100bd2a47a18da
Victor <vlcampos921@gmail.com>
2022-07-28 18:10:06 -0300

    feat: Create Header component and initial styles

---
+++ src/components/header/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 15px;
+  height: 70px;
+  border-bottom: 2px solid lightgray;
+
+  nav {
+    display: none;
+  }
+
+  @media screen and (min-width: 1024px) {
+    nav {
+      width: 50%;
+      display: flex;
+      justify-content: space-evenly;
+    }
+
+    .leftDivNav {
+      width: 50%;
+      height: 100%;
+      display: flex;
+      justify-content: space-evenly;
+      border-right: 3px solid lightgray;
+    }
+  }
+`;
+
+export const StyledMenuButton = styled.button`
+  border: none;
+  background: transparent;
+  font-size: 16px;
+
+  @media screen and (min-width: 1024px) {
+    display: none;
+  }
+`;

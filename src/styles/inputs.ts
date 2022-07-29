b5f1299c3fa6270c655b54de62eb3274a40f23ee
Agnes <agnes.rosa@hotmail.com>
2022-07-29 15:37:58 -0300

    feat: basic input field style

---
+++ src/styles/inputs.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const InputOne = styled.input`
+  height: 38px;
+  border: 1.5px solid #f1f3f5;
+  border-radius: 4px;
+  color: #868e96;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 16px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  :hover {
+    background-color: #f1f3f5;
+  }
+  :focus {
+    border: 1.5px solid #5126ea;
+  }
+`;

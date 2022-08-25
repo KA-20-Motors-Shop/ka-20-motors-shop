72315a06661463f9c37076dd3bca167693ec4899
Agnes <agnes.rosa@hotmail.com>
2022-08-25 16:51:49 -0300

    feat/fix: function getRandomColor made and aplied on component Comments

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,24 +1,17 @@
-import Button from "../button";
+import { IAd } from "../../Interfaces";
 import CommentsCard from "./style";
+import { getInitials } from "../../utils";
 
-interface ICardComments {
-  product: object;
-}
-
-const Comments = ({ product }: ICardComments) => {
+const Comments = (ad: IAd) => {
   return (
     <CommentsCard>
       <h1>Comentários</h1>
-      {/* <div>
-        {product.comments.map((comment) => {
+      <div>
+        {ad.comments.map((comment) => {
           return (
             <div className="commentBox">
               <div className="photoNameDateBox">
-                <img
-                  className="photo"
-                  src={comment.user.photo}
-                  alt={comment.user.name}
-                />
+                <div className="photo">{getInitials(user.name)}</div>
                 <h2 className="name">{comment.user.name}</h2>
                 <div className="dot"></div>
                 <span className="date">{comment.date}</span>
@@ -27,7 +20,7 @@ const Comments = ({ product }: ICardComments) => {
             </div>
           );
         })}
-      </div> */}
+      </div>
     </CommentsCard>
   );
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,12 @@ export const getInitials = (name: string) => {
 
   return result.toUpperCase();
 };
+
+export const getRandomColor = () => {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
